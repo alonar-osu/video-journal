5e0af1817979c377d7706c7af0798af1c48470f1
porygonmon <porygonmon17@gmail.com>
2020-05-22 16:36:25 -0400

    feat: replace MediaController with ExoPlayer for fullscreen video playing

--- app/src/main/java/com/example/android/videojournal/PlayVideoActivity.java
+++ app/src/main/java/com/example/android/videojournal/PlayVideoActivity.java
@@ -3,22 +3,34 @@ package com.example.android.videojournal;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.widget.VideoView;
-import android.widget.MediaController;
 
-import java.io.File;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.ui.PlayerView;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 
 
 public class PlayVideoActivity extends AppCompatActivity {
 
-    MediaController mMediaControls;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Context context = this;
 
         setContentView(R.layout.activity_play_video);
         ActionBar actionBar = getSupportActionBar();
@@ -26,18 +38,30 @@ public class PlayVideoActivity extends AppCompatActivity {
             actionBar.setDisplayHomeAsUpEnabled(true);
         }
 
-        VideoView mPlayVideoView = findViewById(R.id.playVideoView);
+        PlayerView fullscreenPlayVideoView = findViewById(R.id.fullscreenPlayVideoView);
 
-        if (mMediaControls == null) {
-            mMediaControls = new MediaController(PlayVideoActivity.this);
-            mMediaControls.setAnchorView(mPlayVideoView);
-        }
-        mPlayVideoView.setMediaController(mMediaControls);
+        BandwidthMeter bandwidthMeter = new DefaultBandwidthMeter();
+        TrackSelection.Factory videoTracksSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        TrackSelector trackSelector = new DefaultTrackSelector(videoTracksSelectionFactory);
+
+        SimpleExoPlayer videoPlayer = ExoPlayerFactory.newSimpleInstance(context, trackSelector);
+
+        fullscreenPlayVideoView.setUseController(false);
+        fullscreenPlayVideoView.setPlayer(videoPlayer);
+        videoPlayer.setVolume(1f); // volume ON
+
+        fullscreenPlayVideoView.setPlayer(videoPlayer);
+
+        DataSource.Factory dataSourceFactory = new DefaultDataSourceFactory(context, Util.getUserAgent(context, "Video Journal"));
+
         Intent intent = getIntent();
-        String videoPath = intent.getStringExtra("VIDEO_PATH");
-        Uri videoUri = Uri.fromFile(new File(videoPath));
-        mPlayVideoView.setVideoURI(videoUri);
+        String mediaUrl = intent.getStringExtra("VIDEO_PATH");
 
-        mPlayVideoView.start();
+        if (mediaUrl != null) {
+            MediaSource videoSource = new ExtractorMediaSource.Factory(dataSourceFactory)
+                    .createMediaSource(Uri.parse(mediaUrl));
+            videoPlayer.prepare(videoSource);
+            videoPlayer.setPlayWhenReady(true);
+        }
     }
 }
