3f8bf4cf7107fdc888f475ef01be29d9b8ca956f
porygonmon <porygonmon17@gmail.com>
2020-07-14 12:01:26 -0400

    refactor: utilities

--- app/src/main/java/com/example/android/videojournal/utilities/AlarmReceiver.java
+++ app/src/main/java/com/example/android/videojournal/utilities/AlarmReceiver.java
@@ -17,36 +17,16 @@ import androidx.core.content.ContextCompat;
 public class AlarmReceiver extends BroadcastReceiver {
 
     private static final String TAG = AlarmReceiver.class.getSimpleName();
-    static final String CHANNEL_ID_REC_NOTIF = "record_reminder";
-    private int REC_NOTIF_ID = 100;
-
+    private static int REC_NOTIF_ID = 100;
 
     @Override
     public void onReceive(Context context, Intent intent) {
 
         Intent repeatingIntent = new Intent(context, DailyVideoFeedActivity.class);
-        repeatingIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); //called activity will replace current
-
-        PendingIntent pendingIntent = PendingIntent.getActivity(context, REC_NOTIF_ID, repeatingIntent, PendingIntent.FLAG_UPDATE_CURRENT);
-
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID_REC_NOTIF)
-                .setColor(ContextCompat.getColor(context, R.color.colorPrimary))
-                .setSmallIcon(R.drawable.ic_create_entry)
-                .setContentTitle("Time for Video Journal Entry")
-                .setContentText("Let's take a new daily video")
-                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
-                .setContentIntent(pendingIntent)
-                .setAutoCancel(true);
-
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.JELLY_BEAN
-                && Build.VERSION.SDK_INT < Build.VERSION_CODES.O) {
-            builder.setPriority(NotificationCompat.PRIORITY_HIGH);
-        }
-
-        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
-
-        notificationManager.notify(REC_NOTIF_ID, builder.build());
+        repeatingIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        PendingIntent pendingIntent = PendingIntent.getActivity(context, REC_NOTIF_ID,
+                repeatingIntent, PendingIntent.FLAG_UPDATE_CURRENT);
+        NotificationUtils.showNotification(context, pendingIntent);
     }
 
-    }
+}
--- app/src/main/java/com/example/android/videojournal/utilities/NotificationUtils.java
+++ app/src/main/java/com/example/android/videojournal/utilities/NotificationUtils.java
@@ -9,29 +9,31 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Build;
 import android.preference.PreferenceManager;
-import android.widget.Toast;
 
 import com.example.android.videojournal.R;
-import com.example.android.videojournal.utilities.AlarmReceiver;
-
 import java.util.Calendar;
+
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
+import androidx.core.content.ContextCompat;
 
 import static android.content.Context.ALARM_SERVICE;
 
 public class NotificationUtils {
 
-    static final String CHANNEL_ID_REC_NOTIF = "record_reminder";
+    private static final String CHANNEL_ID_REC_NOTIF = "record_reminder";
     private static int REC_NOTIF_ID = 100;
+    private static final int MINUTES_IN_HOUR = 60;
+    private static final int DEFAULT_NOTIF_PREF_VALUE = 725;
 
     public static void createNotificationChannel(Context context) {
-        // create channel only on API 26+ otherwise not in support library
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) { // channel on API 26+ otherwise not in support library
             CharSequence name = context.getString(R.string.channel_name);
             String description = context.getString(R.string.channel_description);
             int importance = NotificationManager.IMPORTANCE_DEFAULT;
             NotificationChannel channel = new NotificationChannel(CHANNEL_ID_REC_NOTIF, name, importance);
             channel.setDescription(description);
-            //register channel with system
+
             NotificationManager notificationManager = (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
             notificationManager.createNotificationChannel(channel);
         }
@@ -41,8 +43,8 @@ public class NotificationUtils {
         Calendar calendar = Calendar.getInstance();
         calendar.set(Calendar.HOUR_OF_DAY, hour);
         calendar.set(Calendar.MINUTE, minute);
-        // DEBUG
-        Toast.makeText(context, "Reminder notif: time set at hours= " + hour + " and min= " + minute, Toast.LENGTH_LONG).show();
+
+        // Toast.makeText(context, "Notification set at hours= " + hour + " and min= " + minute, Toast.LENGTH_SHORT).show();
         Intent intent = new Intent(context, receiver);
         PendingIntent pendingIntent = PendingIntent.getBroadcast(context, REC_NOTIF_ID, intent, 0);
 
@@ -53,13 +55,29 @@ public class NotificationUtils {
     public static void updateNotificationTime(Context context) {
         SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
         // if activate checkmark is on
-        if (sharedPreferences.getBoolean(context.getString(R.string.pref_activate_reminder_key), context.getResources().getBoolean(R.bool.pref_activate_reminder_default))) {
-            int minutesAfterMidnight = sharedPreferences.getInt(context.getString(R.string.pref_reminder_time_key), 60);
-            // get hours and mins from savedTime
-            int hours = minutesAfterMidnight / 60;
-            int minutes = minutesAfterMidnight % 60;
+        if (sharedPreferences.getBoolean(context.getString(R.string.pref_activate_reminder_key),
+                context.getResources().getBoolean(R.bool.pref_activate_reminder_default))) {
+
+            int minutesAfterMidnight = sharedPreferences.getInt(context.getString(R.string.pref_reminder_time_key),
+                    DEFAULT_NOTIF_PREF_VALUE);
+            int hours = minutesAfterMidnight / MINUTES_IN_HOUR;
+            int minutes = minutesAfterMidnight % MINUTES_IN_HOUR;
             setUpReminderNotification(context, hours, minutes, AlarmReceiver.class);
         }
+    }
+
+    public static void showNotification(Context context, PendingIntent pendingIntent) {
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context, CHANNEL_ID_REC_NOTIF)
+                .setColor(ContextCompat.getColor(context, R.color.colorPrimary))
+                .setSmallIcon(R.drawable.ic_create_entry)
+                .setContentTitle("Time for Video Journal Entry")
+                .setContentText("Let's take a new daily video")
+                .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+                .setContentIntent(pendingIntent)
+                .setAutoCancel(true);
+
+        NotificationManagerCompat notificationManager = NotificationManagerCompat.from(context);
+        notificationManager.notify(REC_NOTIF_ID, builder.build());
     }
 
 }
